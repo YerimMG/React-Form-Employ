58a3340e8eac60bdf9c9628a781a4a634425a4f5
YERIM <mac@Yerim-4.local>
2020-01-15 00:05:36 -0600

    adding fade-in

---
+++ src/views/usersList/UserList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserList() {
+  return (
+    <div className="fade-in-slow">
+      <h1>Hola desde user List </h1>
+    </div>
+  );
+}
+
+export default UserList;
