234567009efb20f32140154ddb717a83e8229b7e
YERIM <mac@Yerim-4.local>
2020-01-15 00:05:11 -0600

    fixing Navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
-function navbar() {
+function useNavbar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="sm">
-      <Navbar.Brand className="title" href="#home">
+    <Navbar className="fade-in-fast" bg="dark" variant="dark" expand="sm">
+      <Navbar.Brand className="title">
         Lista de <span>Empleados</span>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className=" mr-auto">
-          <Nav.Link className="nav-button" href="#register">
+          <Nav.Link as={Link} to="/" className="nav-button" href="/#">
             Registro
           </Nav.Link>
-          <Nav.Link className="nav-button" href="#listas">
+          <Nav.Link as={Link} to="/list" className="nav-button" href="#/">
             Listas
           </Nav.Link>
         </Nav>
@@ -23,4 +24,4 @@ function navbar() {
   );
 }
 
-export default navbar;
+export default useNavbar;
